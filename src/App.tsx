118ae2d444c7d21d5f0e9751a64b9bcc3d7deed3
sohseyedi <sohseyedi@gmail.com>
2023-11-05 10:37:38 +0330

    navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Layout from "./components/Layout";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Layout />} />
       </Routes>
-      
+      <Toaster/>
     </BrowserRouter>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,23 @@
-import { FC } from 'react'
-import * as RiIcon from 'react-icons/ri'
+import { FC } from "react";
+import * as IoIcon from "react-icons/io5";
 
-const Navbar:FC = () => {
+const Navbar: FC = () => {
   return (
-    
-  )
-}
+    <nav className="flex items-center justify-between max-w-7xl mx-auto px-3 md:px-0 py-4">
+      <div className="flex items-center gap-x-2">
+        <IoIcon.IoFootballOutline size={40} className="text-[#26abb8]" />
+        <h1 className="text-3xl font-bold text-[#26abb8] ">فوتلایو</h1>
+      </div>
+      <div className="flex items-center justify-center gap-x-2">
+        <button className="navBtn hover:rotate-180 rotate-0 hover:bg-transparent hover:text-[#26abb8]">
+          <IoIcon.IoSettingsOutline size={26} />
+        </button>
+        <button className="navBtn icon">
+          <IoIcon.IoNotificationsOutline size={26} />
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
